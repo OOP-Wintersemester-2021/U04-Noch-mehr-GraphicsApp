b010f4a4680253e9525e2846c0e3c99860e5ae89
luggaesch <lukas.schauhuber1@gmail.com>
2020-10-31 15:11:58 +0100

    Starterpaket hinzugefügt

---
+++ src/TargetWithMethods/TargetLauncher.java
@@ -0,0 +1,11 @@
+package TargetWithMethods;
+
+import de.ur.mi.oop.launcher.GraphicsAppLauncher;
+
+
+public class TargetLauncher {
+
+    public static void main(String[] args) {
+        GraphicsAppLauncher.launch("Target");
+    }
+}
