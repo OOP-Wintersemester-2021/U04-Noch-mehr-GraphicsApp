533049261ca55802c08771f9879e4fa6a337cb32
luggaesch <lukas.schauhuber1@gmail.com>
2020-10-31 15:14:51 +0100

    Lösung hinzugefügt

---
+++ src/BouncingBall/BouncingBallLauncher.java
@@ -0,0 +1,11 @@
+package BouncingBall;
+
+import de.ur.mi.oop.launcher.GraphicsAppLauncher;
+
+
+public class BouncingBallLauncher {
+
+    public static void main(String[] args) {
+        GraphicsAppLauncher.launch("BouncingBall");
+    }
+}
